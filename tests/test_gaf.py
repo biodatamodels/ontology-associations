f37cfd2766ca69b2b44b5ab19a0d7c0b290df97a
cmungall <cjm@berkeleybop.org>
2021-02-13 18:19:16 -0800

    gpad

--- tests/test_gaf.py
+++ tests/test_gaf.py
@@ -3,6 +3,13 @@ from ontology_association.datamodel.gaf import GafAssociation
 def test_gaf():
     assoc = GafAssociation(
         db='UniProtKB',
-        local_id='P1234'
+        local_id='P1234',
+        db_object_symbol='abc',
+        ontology_class_ref='GO:0001234',
+        db_object_taxon='NCBITaxon:9606',
+        evidence_type='IEA',
+        references=['PMID:123456'],
+        assigned_by='MGI',
+        annotation_date='1999-01-01'
     )
     
